eebce67694482cd9b844bca353cf5a012f03e7f5
KevinRichard1 <kevin.rohit@gmail.com>
2024-12-15 19:13:39 -0500

    Added sidebar for mobile view and contact page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Homepage from './Pages/Homepage';
 import Project from './Pages/Project';
 import { Footer } from './Components/Footer/Footer';
+import Contact from './Pages/Contact';
+import Sidebar from './Components/Navbar/Sidebar';
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false)
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar/>
+        <Navbar setMenuStatus={setIsMenuOpen}/>
+        <Sidebar isOpen={isMenuOpen}/>
         <Routes>
           <Route path ='/' element={<Homepage/>}/>
           <Route path ='/projects' element={<Project/>}/>
+          <Route path ='/contact' element={<Contact/>}/>
         </Routes>
         <Footer/>
       </BrowserRouter>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,37 +1,51 @@
-import './Navbar.css'
-import React, { useState, useEffect, useRef } from 'react'
-import { NavLink } from 'react-router-dom'
-import logo from '../Assets/favicon.png'
+import './Navbar.css';
+import React, { useState, useEffect, useRef } from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../Assets/favicon.png';
 
-const Navbar = () => {
-    const [menu, setMenu] = useState("home");
-    const [scrollDirection, setScrollDirection] = useState("up");
-    const lastScrollTop = useRef(0);
+const Navbar = ({ setMenuStatus }) => {
+  const [menu, setMenu] = useState("home");
+  const [scrollDirection, setScrollDirection] = useState("up");
+  const lastScrollTop = useRef(0);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const currentScrollPos = window.pageYOffset;
-            if (currentScrollPos > lastScrollTop.current) {
-                setScrollDirection("down");
-            } else {
-                setScrollDirection("up");
-            }
-            lastScrollTop.current = currentScrollPos <= 0 ? 0 : currentScrollPos;
-        };
+  const toggleMenu = () => {
+    setMenuOpen(prevState => !prevState);
+    setMenuStatus(prevState => !prevState);
+  };
 
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, );
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      if (currentScrollPos > lastScrollTop.current) {
+        setScrollDirection("down");
+      } else {
+        setScrollDirection("up");
+      }
+      lastScrollTop.current = currentScrollPos <= 0 ? 0 : currentScrollPos;
+    };
 
-    return (
-        <div className={`navbar ${scrollDirection === "up" ? "show-navbar" : "hide-navbar"}`}>
-            <img src={logo} alt="" />
-            <ul className="nav-menu">
-                <li onClick={()=>{setMenu("home")}}><NavLink style={{textDecoration: 'none'}} to = '/'>Home</NavLink>{menu === "home"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("projects")}}><NavLink style={{textDecoration: 'none'}} to = '/projects'>Projects</NavLink>{menu === "projects"?<hr/>:<></>}</li>
-            </ul>
-        </div>
-    )
-}
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
-export default Navbar
+  return (
+    <div className={`navbar ${scrollDirection === "up" ? "show-navbar" : "hide-navbar"}`}>
+      <NavLink to="/" className='navbar-logo'>
+        <img src={logo} alt="" />
+      </NavLink>
+      <ul className="nav-menu">
+        <li onClick={() => { setMenu("home"); }}><NavLink style={{ textDecoration: 'none' }} to='/'>Home</NavLink>{menu === "home" ? <hr /> : <></>}</li>
+        <li onClick={() => { setMenu("projects"); }}><NavLink style={{ textDecoration: 'none' }} to='/projects'>Projects</NavLink>{menu === "projects" ? <hr /> : <></>}</li>
+        <li onClick={() => { setMenu("contact"); }}><NavLink style={{ textDecoration: 'none' }} to='/contact'>Contact</NavLink>{menu === "contact" ? <hr /> : <></>}</li>
+      </ul>
+      <div className={`navbar-dropdown ${menuOpen ? 'change' : ''}`} onClick={toggleMenu}>
+        <div className="bar1"></div>
+        <div className="bar2"></div>
+        <div className="bar3"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
